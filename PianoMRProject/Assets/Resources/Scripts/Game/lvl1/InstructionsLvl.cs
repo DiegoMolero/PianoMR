528d0b5aed522bc7a2da3e1197cf598ac92d7dda
DiegoMolero <diego.molero@alu.uclm.es>
2018-05-07 12:18:46 +0200

    Changes in the instructions given to the player

--- PianoMRProject/Assets/Resources/Scripts/Game/lvl1/InstructionsLvl.cs
+++ PianoMRProject/Assets/Resources/Scripts/Game/lvl1/InstructionsLvl.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class InstructionsLvl : MonoBehaviour {
     public GameObject game;
-    public TextMesh txt;
+    public GameObject InstructionFingers;
+    public GameObject InstructionKeys;
     public float Timer = 6.0f;
+    public TextMesh txt;
+    private int InstructionState;
+
+    private GameObject aux;
+    private float limit_timer;
+
     private int aux_timer;
     // Use this for initialization
     void Start () {
+        limit_timer = Timer;
         aux_timer = (int)Timer;
-	}
+        InstructionState = 0;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -19,6 +28,29 @@ public class InstructionsLvl : MonoBehaviour {
         {
             aux_timer = (int)Timer;
             txt.text = "Starts in \n" + aux_timer;
+            if (aux_timer > limit_timer* 2/3)
+            {
+                if( InstructionState == 0)
+                {
+                    InstructionState++;
+                    aux = Instantiate(InstructionKeys, this.transform);
+                    aux.name = aux.transform.name.Replace("(Clone)", "");
+
+                }
+
+            }
+            else if(aux_timer < limit_timer * 2/3)
+            {
+                if (InstructionState == 1)
+                {
+                    InstructionState++;
+                    Destroy(aux);
+                    aux = Instantiate(InstructionFingers, this.transform);
+                    aux.name = aux.transform.name.Replace("(Clone)", "");
+                }
+
+            }
+
             if (aux_timer == 0)
             {
                 GameObject lvl_aux = Instantiate(game);
